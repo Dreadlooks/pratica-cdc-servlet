dae5174070ee6f9ae5902e4ca9a4aad68506b003
Gabriel Ronei <gabriheron@hotmail.com>
2020-02-26 19:27:58 -0300

    [CODE REVIEW] Apagando método que eu tinha esquecido

--- src/br/com/caelum/cdc/shared/StringConverter.java
+++ src/br/com/caelum/cdc/shared/StringConverter.java
@@ -3,15 +3,8 @@ package br.com.caelum.cdc.shared;
 public class StringConverter implements Converter<String> {
 
 	@Override
-	public Class<?> getType() {
-		// TODO Auto-generated method stub
-		return String.class;
-	}
-
-	@Override
 	public String convert(String value) {
 		// TODO Auto-generated method stub
 		return value;
 	}
-
 }
